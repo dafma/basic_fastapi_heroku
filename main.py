ebfd81bfb04322ec9030f32ccc6609853a72787c
dafma <mar_tp@hotmail.es>
2021-08-04 10:48:57 -0500

    base

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
+#otro
 
 @app.get("/")
 def home():
